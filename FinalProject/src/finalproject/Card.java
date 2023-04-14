fc10f9a48bef9971dd4dcb6b2fa30bd25a37872a
CalebH7 <harrcale@sheridancollege.ca>
2023-04-14 00:01:29 -0400

    Card file is refactored with addition classes (e.g., NumberCard, ActionCard, etc.).

--- FinalProject/src/finalproject/Card.java
+++ FinalProject/src/finalproject/Card.java
@@ -21,5 +21,93 @@ public abstract class Card {
      */
     @Override
     public abstract String toString();
+    
+    protected enum Colour{
+        BLUE, GREEN, RED, YELLOW;
+    }
+    protected enum Value{
+        ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE;
+    }
 
+    protected enum Action{
+        SKIP, REVERSE, PLUS_TWO, WILD, PLUS_FOUR;
+    }
 }
+
+class NumberCard extends Card{
+    private final Colour colour;
+    private final Value value;
+
+    public NumberCard(){
+        int colour = (int)(Math.random() * 4);
+        this.colour = Colour.values()[colour];
+        int value = (int)(Math.random() * 10);
+        this.value = Value.values()[value];
+    }
+    
+    public String getColour(){
+        return colour.toString();
+    }
+    
+    public String getValue(){
+        return value.toString();
+    }
+
+    @Override
+    public String toString(){
+        return colour.toString() + "-" + value.toString();
+    }
+}
+
+abstract class ActionCard extends Card{
+    protected Action action;
+    
+    @Override
+    public abstract String toString();
+}
+
+class ColouredActionCard extends ActionCard{
+    private Colour colour;
+    
+    public ColouredActionCard(){
+        int action = (int)(Math.random() * 3);
+        this.action = Action.values()[action];
+        int colour = (int)(Math.random() * 4);
+        this.colour = Colour.values()[colour];
+    }
+    
+    public String getColour(){
+        return colour.toString();
+    }
+    
+    public String getAction(){
+        return action.toString();
+    }
+    
+    @Override
+    public String toString(){
+        return colour.toString() + "-" + action.toString();
+    }
+}
+
+class WildActionCard extends ActionCard{
+    private Colour colour;
+    
+    public WildActionCard(){
+        int action = (int)(Math.random() * 2) + 3;
+    }
+    
+    public String getColour(){
+        return colour.toString();
+    }
+    
+    public void setColour(Colour colour){
+        this.colour = colour;
+    }
+    
+    @Override
+    public String toString(){
+        return colour.toString() + "-" + action.toString();
+    }
+}
+
