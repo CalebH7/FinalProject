4d4643fa5ad209471ab5bcbaf163a745482d883c
CalebH7 <harrcale@sheridancollege.ca>
2023-04-14 21:38:31 -0400

    The Uno subclass was created for the abstract Game class.

---
+++ FinalProject/src/finalproject/Uno.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package finalproject;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Inven
+ */
+public class Uno extends Game{
+    private GroupOfCards mainDeck = new GroupOfCards(108);
+    
+    public Uno(){
+        super("Uno");
+        
+    }
+    
+    public void play(){
+        
+    }
+    
+    public void declareWinner(){
+        
+    }
+
+}
